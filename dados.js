ca387b27fdb0d70082fb7aadfef569207fe2ef87
Damaris Ribeiro de Morais França <128935851+DamarisJs@users.noreply.github.com>
2024-09-07 20:15:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Pudim de leite em pó",
+        descricao : "O pudim é uma sobremesa clássica e amada por muitos, não é mesmo? Sua textura cremosa, sabor adocicado e versatilidade na hora de combinar com outrosingredientes o tornam um verdadeiro clássico da culinária.",
+        link : "https://www.youtube.com/watch?v=1Y6Y2jk8Ra8&t=1s",
+        tags: "pudim sobremesa doce"
+    },
+
+    {
+        titulo : "Massa de panqueca",
+        descricao : "A panqueca salgada é um verdadeiro coringa na cozinha, né? Com ela, podemos criar diversas combinações deliciosas e nutritivas, do café da manhã ao jantar.",
+        link : "https://www.youtube.com/watch?v=-wANfHO6i9U&t=24s",
+        tags: "almoço jantar panqueca salgado"
+    },
+
+    {
+        titulo : "Leite condensado fake",
+        descricao : "O leite condensado fake, também conhecido como leite condensado vegano ou light, tem ganhado cada vez mais espaço nas prateleiras dos supermercados e nos corações dos consumidores que buscam alternativas mais saudáveis e com menos calorias.",
+        link : "https://www.youtube.com/watch?v=MpErfNhBXFI",
+        tags: "sobremesa doce condensado"
+    },
+
+]
+
